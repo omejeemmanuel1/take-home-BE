40b4fa338efa40833317185c5124e6ba804aa4a9
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2023-07-08 12:45:40 +0100

    Backend Completed

--- src/config/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-// import * as admin from 'firebase-admin';
-// // path to service account
-// import * as serviceAccount from './ServiceAccountKey.json';
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
-// });
-
-// export default admin;
